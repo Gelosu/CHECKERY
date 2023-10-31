1b238d672142114496f00c480e4d44fe8f3fcd3a
Gelosu <118784451+Gelosu@users.noreply.github.com>
2023-11-01 01:20:22 +0800

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,33 +1,11 @@
-import React, { useState } from "react";
 import Footer from "@/components/DefaultFix/Footer";
 import NavBar from "@/components/DefaultFix/NavBar";
 import Login from "@/components/LoginPage/Login";
 
-export default function LoginPage() {
-  const [isConnected, setIsConnected] = useState(null);
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  const checkDatabaseConnection = async () => {
-    try {
-      const response = await fetch('/checkDatabaseConnection');
-      if (response.ok) {
-        const data = await response.json();
-        setIsConnected(data.connected);
-        if (data.connected) {
-          window.alert("Connected to the database");
-        } else {
-          window.alert("Not connected to the database");
-        }
-      } else {
-        setIsConnected(false);
-        window.alert("Failed to check the database connection.");
-      }
-    } catch (error) {
-      console.error('Error checking database connection:', error);
-      setIsConnected(false);
-      window.alert("An error occurred while checking the database connection.");
-    }
-  };
-
+export default async function LoginPage() {
+  await wait(1000)
   return (
     <main>
       <div>
@@ -35,14 +13,6 @@ export default function LoginPage() {
       </div>
       <div>
         <Login />
-        <button onClick={checkDatabaseConnection}>Check Database Connection</button>
-        {isConnected === null ? (
-          <p>Click the button to check the connection.</p>
-        ) : isConnected ? (
-          <p>Connected to the database</p>
-        ) : (
-          <p>Not connected to the database</p>
-        )}
       </div>
       <div>
         <Footer />

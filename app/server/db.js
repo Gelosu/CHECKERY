9fe9a7e1079ca7d2e09a94a421243c477af9a9fa
Gelosu <118784451+Gelosu@users.noreply.github.com>
2023-11-01 11:04:56 +0800

    Update db.js

--- app/server/db.js
+++ app/server/db.js
@@ -3,14 +3,13 @@ const app = express();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: bzn5olpirsjaxyczya13-mysql.services.clever-cloud.com,
-  user: ur9qdorhytfru6zr,
-  password: 0SKn6yyEjjNfJ9DcSs4Q,
-  database: bzn5olpirsjaxyczya13,
+  host: 'bzn5olpirsjaxyczya13-mysql.services.clever-cloud.com',
+  user: 'ur9qdorhytfru6zr',
+  password: '0SKn6yyEjjNfJ9DcSs4Q',
+  database: 'bzn5olpirsjaxyczya13',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  port: 3306,
 });
 
 // Test the database connection
